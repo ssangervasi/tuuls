658ebeeff7131f77df1d0e6770248b76169a8c14
Sebastian Sangervasi <villain@harmless.dev>
2021-08-12 15:16:03 -0400

    fun macro expr replacement

--- liib/src/lib.rs
+++ liib/src/lib.rs
@@ -1,1 +1,41 @@
 pub const MSG: &str = "Das library";
+
+#[macro_export]
+macro_rules! replace_expr {
+    ($_t:tt $sub:expr) => {
+        $sub
+    };
+}
+
+#[macro_export]
+macro_rules! count_tts {
+    ($($tts:tt)*) => {<[()]>::len(&[$(liib::replace_expr!($tts ())),*])};
+}
+
+#[macro_export]
+macro_rules! revec {
+	( $( $x:expr ),* ) => {
+		{
+			// let mut temp_vec = Vec::new();
+			// $(
+			// 	temp_vec.push($x);
+			// )*
+			// $(
+			// 	temp_vec.push($x);
+			// )?
+			// temp_vec.reverse();
+			// let mut size = 0;
+			// $(
+			// 	 $x;
+			// 	size += 1;
+			// )*
+			let mut size = liib::count_tts!($($x)*);
+			println!("size={:?}", size);
+			let mut temp_vec = Vec::with_capacity(size);
+			$(
+				temp_vec.insert(0, $x);
+			)*
+			temp_vec
+		}
+	};
+}
--- tuul/src/main.rs
+++ tuul/src/main.rs
@@ -2,4 +2,13 @@ use liib;
 
 fn main() {
     println!("{}", liib::MSG);
+
+    // let v: Vec<u8> = liib::revec![1, 2, 3];
+    let v: Vec<u8> = liib::revec![n(1), n(2), 3];
+    print!("{:?}", v)
+}
+
+fn n(ni: u8) -> u8 {
+    println!("n({})", ni);
+    ni
 }
