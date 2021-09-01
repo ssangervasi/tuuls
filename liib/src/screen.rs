1710a3dddf6ee70cf1d98435bf08ded0b9366562
Sebastian Sangervasi <villain@harmless.dev>
2021-09-01 16:31:24 -0700

    buffering writes

--- liib/src/screen.rs
+++ liib/src/screen.rs
@@ -2,22 +2,19 @@ use std::collections::HashMap;
 
 use crate::position::{Position, Visible};
 
-const BLANK: char = ' ';
+pub const BLANK: char = ' ';
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct Screen {
     pub cols: i32,
     pub rows: i32,
-    hash_map: HashMap<Position, char>,
+    written: HashMap<Position, char>,
+    buffer: HashMap<Position, char>,
 }
 
 impl Default for Screen {
     fn default() -> Self {
-        Self {
-            cols: 100,
-            rows: 100,
-            hash_map: HashMap::with_capacity(10 * 10),
-        }
+        Self::with_size((100, 100).into())
     }
 }
 
@@ -26,30 +23,79 @@ impl Screen {
         Self {
             cols: col_row.col,
             rows: col_row.row,
-            hash_map: HashMap::with_capacity(10 * 10),
+            written: HashMap::with_capacity(10 * 10),
+            buffer: HashMap::with_capacity(10 * 10),
         }
     }
 
+    /**
+     * Inserts the character into the buffer. Until the screen is [#flush]-ed the written
+     * value will not be returned by [#read]. Returns the character that was previously
+     * buffered, or [BLANK].
+     */
     pub fn write(&mut self, position: &Position, ch: char) -> char {
-        match ch {
-            BLANK => self.hash_map.remove(position),
-            _ => self.hash_map.insert(*position, ch),
-        }
-        .unwrap_or(BLANK)
+        self.buffer.insert(*position, ch).unwrap_or(BLANK)
     }
 
+    /**
+     * Returns the character that has been written and flushed at the designated position.
+     * If the position has not been touched, [BLANK] is returned.
+     */
     pub fn read(&self, position: &Position) -> char {
-        match self.hash_map.get(position) {
+        match self.written.get(position) {
             Some(&ch) => ch,
             None => BLANK,
         }
     }
 
-    pub(crate) fn mem(&self) -> usize {
-        (std::mem::size_of::<Position>() + std::mem::size_of::<char>()) * self.hash_map.len()
+    /**
+     * Flushes the buffered writes into the written state and returns copies of the elements
+     * that were written.
+     */
+    pub fn flush(&mut self) -> Vec<(Position, char)> {
+        let mut updates: Vec<(Position, char)> = Vec::with_capacity(self.buffer.capacity());
+        for (&position, &ch) in self.buffer.iter() {
+            let original = self.written.insert(position, ch);
+            if original == None || original != Some(ch) {
+                updates.push((position, ch));
+            }
+        }
+        updates
     }
+
+    // pub(crate) fn mem(&self) -> usize {
+    //     (std::mem::size_of::<Position>() + std::mem::size_of::<char>()) * self.written.len()
+    // }
 
     pub fn clip(&self, position: &Position) -> Visible {
         (*position).into()
     }
+}
+
+#[macro_export]
+macro_rules! scrite {
+    ($s:expr, $( $x:expr ),+ ) => {
+        let screen: &mut Screen = $s;
+        $(
+            {
+                let (c, r, ch): (i32, i32, char) = $x;
+                screen.write(&(c, r).into(), ch);
+            }
+        )+
+    };
+}
+
+#[test]
+fn test_buffer() {
+    let mut screen = Screen::default();
+    scrite!(&mut screen, (0, 1, 'h'), (1, 2, 'i'));
+
+    assert_eq!(screen.read(&(0, 1).into()), ' ');
+    assert_eq!(screen.read(&(1, 2).into()), ' ');
+
+    let out = screen.flush();
+    assert_eq!(out.len(), 2);
+
+    assert_eq!(screen.read(&(0, 1).into()), 'h');
+    assert_eq!(screen.read(&(1, 2).into()), 'i');
 }
