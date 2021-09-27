1cdf1a0ea3fec1293c566498e8c0e9ef69081758
Sebastian Sangervasi <villain@harmless.dev>
2021-09-27 13:13:05 -0700

    not impossible

--- laang/src/lib.rs
+++ laang/src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io;
 use std::io::BufRead;
+use std::mem::discriminant;
 use std::path::Path;
 
 use lazy_static::lazy_static;
@@ -15,14 +16,14 @@ pub fn eval(opts: &CliOptions) {
     let fpath = Path::new(&opts.path);
     let f = fs::File::open(fpath).unwrap();
     let reader = io::BufReader::new(f);
-    let mut parser = Parser::new();
+    let mut tokenizer = Tokenizer::new();
     for (i, line_r) in reader.lines().enumerate() {
         if let Ok(line) = line_r {
             println!("{}:\t{}", i, line);
-            parser.take_line(&line);
+            tokenizer.take_line(&line);
         }
     }
-    println!("{:?}", parser)
+    println!("{:?}", tokenizer)
 }
 
 lazy_static! {
@@ -41,7 +42,6 @@ enum TokenType {
 enum Context {
     Single(TokenType, String),
     Multi(TokenType, String),
-    Bare(String),
     Empty,
 }
 
@@ -49,29 +49,48 @@ impl Context {
     fn combine(&self, other: &Self) -> (Self, Self) {
         use Context::*;
         match (self, other) {
-            (Multi(s_t, s_s), Multi(o_t, o_s)) => match s_t {
-                o_t => {
+            (Multi(s_t, s_s), Multi(o_t, o_s)) => {
+                if discriminant(s_t) == discriminant(o_t) {
                     let mut s = String::new();
                     s.push_str(s_s);
                     s.push_str(o_s);
                     (Empty, Multi(*s_t, s))
+                } else {
+                    (self.clone(), other.clone())
                 }
-                _ => (self, other),
-            },
-            _ => (self, other),
+            }
+            _ => (self.clone(), other.clone()),
         }
     }
 }
 
 #[derive(Debug)]
-struct Parser {
+struct Tokenizer {
     tokens: Vec<String>,
 }
 
-impl Parser {
+impl Tokenizer {
     fn new() -> Self {
         Self {
             tokens: Vec::with_capacity(1000),
+        }
+    }
+
+    fn take_line(&mut self, line: &str) {
+        let gs = UnicodeSegmentation::graphemes(line, true).collect::<Vec<&str>>();
+        let mut context = Context::Empty;
+        for g in gs {
+            let current = if BRACKET.is_match(g) {
+                Context::Single(TokenType::Bracket, g.to_string())
+            } else if SPACE.is_match(g) {
+                Context::Multi(TokenType::Space, g.to_string())
+            } else {
+                Context::Multi(TokenType::Any, g.to_string())
+            };
+
+            let (complete, incomplete) = context.combine(&current);
+            self.push(&complete);
+            context = incomplete;
         }
     }
 
@@ -84,38 +103,7 @@ impl Parser {
             Multi(_, s) => {
                 self.tokens.push(s.to_string());
             }
-            Bare(s) => {
-                self.tokens.push(s.to_string());
-            }
             Empty => {}
-        }
-    }
-
-    fn take_line(&mut self, line: &str) {
-        let gs = UnicodeSegmentation::graphemes(line, true).collect::<Vec<&str>>();
-        let mut context = Context::Empty;
-        for g in gs {
-            let current = if BRACKET.is_match(g) {
-                Context::Single(TokenType::Bracket, g.to_string())
-            } else if SPACE.is_match(g) {
-                Context::Multi(TokenType::Bracket, g.to_string())
-            } else {
-                Context::Bare(g.to_string())
-            };
-
-            let (complete, incomplete) = context.combine(&current);
-            self.push(&complete);
-            context = incomplete;
-
-            // if BRACKET.is_match(g) {
-            //     if !cur.is_empty {
-            //         self.tokens.push(cur);
-            //         cur = String::new();
-            //     }
-            //     self.tokens.push(g.to_string());
-            // } else {
-            //     cur.push_str(g)
-            // }
         }
     }
 }
